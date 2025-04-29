57e3a1660d0345ff4b8e54ef435e8cf8af23f88c
Mindul <pencake33@naver.com>
2025-04-29 17:58:05 +0900

    nextauth 덜어내고, 백엔드에 책임을 전가하기로 했습니다.

--- src/components/auth/SignOutButton.tsx
+++ src/components/auth/SignOutButton.tsx
@@ -1,11 +1,20 @@
-import { signOut } from "@/auth"
- 
-export default function SignOutButton() {
+import { redirect } from "next/navigation";
+
+ export default function SignOutButton() {
   return (
     <form
       action={async () => {
         "use server"
-        await signOut({redirectTo:'/'})
+        const res=await fetch(`${process.env.BACKEND_URL}/api/auth/logout/kakao`, {
+          method:"POST",
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          credentials: 'include',
+          body: JSON.stringify({})
+        });
+        console.log(res);
+        console.log("응애");
       }}
     >
       <button type="submit">Sign Out</button>
--- src/type/env.d.ts
+++ src/type/env.d.ts
@@ -1,8 +1,10 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
-      KAKAO_CLIENT_ID: string;
-      KAKAO_CLIENT_SECRET: string;
+      AUTH_KAKAO_ID: string;
+      AUTH_KAKAO_SECRET: string;
+      BACKEND_URL: string;
+      BACKEND_AUTH_KAKAO_URL: string;
     }
   }
   // interface Window {
