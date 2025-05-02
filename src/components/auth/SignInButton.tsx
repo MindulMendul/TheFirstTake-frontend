acaf22110d0face84184612ea735d05e2cc8f1d1
Mindul <pencake33@naver.com>
2025-05-02 21:22:41 +0900

    cookie 제대로 박힐... 걸?

--- src/components/auth/SignInButton.tsx
+++ src/components/auth/SignInButton.tsx
@@ -1,13 +1,9 @@
-import { redirect } from "next/navigation";
 
 export default function SignInButton() {
   return (
     <form
-      action={async () => {
-        "use server"
-        const uri=`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.AUTH_KAKAO_ID}&redirect_uri=${"http://172.16.101.234:3000/api/auth/callback/kakao"}&response_type=code`;
-        redirect(uri);
-      }}
+      action={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.AUTH_KAKAO_ID}&redirect_uri=${process.env.AUTH_KAKAO_REDIRECT_URL}&response_type=code`}
+      method="GET"
     >
       <button type="submit">Sign In</button>
     </form>
--- src/type/env.d.ts
+++ src/type/env.d.ts
@@ -1,10 +1,11 @@
 declare global {
   namespace NodeJS {
     interface ProcessEnv {
+      BACKEND_URL: string;
       AUTH_KAKAO_ID: string;
       AUTH_KAKAO_SECRET: string;
-      BACKEND_URL: string;
-      BACKEND_AUTH_KAKAO_URL: string;
+      AUTH_KAKAO_REDIRECT_URL: string;
+      AUTH_KAKAO_BACKEND_URL: string;
     }
   }
   // interface Window {
