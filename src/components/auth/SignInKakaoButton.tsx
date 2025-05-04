69aa608a08d7cd0a6218281b551f02d5725e121c
Mindul <pencake33@naver.com>
2025-05-04 23:10:54 +0900

    간단한 크래댄셜 로그인 기능 구색 맞추기만 해둠

--- src/components/auth/SignInKakaoButton.tsx
+++ src/components/auth/SignInKakaoButton.tsx
@@ -1,10 +1,10 @@
-export default function SignInButton() {
+export default function SignInKakaoButton() {
   return (
     <form
       action={`https://kauth.kakao.com/oauth/authorize?client_id=${process.env.AUTH_KAKAO_ID}&redirect_uri=${process.env.AUTH_KAKAO_REDIRECT_URL}&response_type=code`}
       method="GET"
     >
-      <button type="submit">Sign In</button>
+      <button type="submit">Kakao Sign In</button>
     </form>
   )
 }
