5e3b0707da3d3bb5ff9960218e559e9ce9f75864
Mindul <pencake33@naver.com>
2025-05-11 22:29:59 +0900

    간단하게 더미 API 연결해둠

--- src/app/basic/page.tsx
+++ src/app/basic/page.tsx
@@ -1,12 +1,39 @@
 "use client"
 
+import { postBasicInfo } from "@/apis/API";
 import Dropdown from "@/components/Dropdown";
 import DropdownOption from "@/components/DropdownOption";
 import ThemeButton from "@/components/ThemeButton";
 import { useRouter } from "next/navigation";
+import { useState } from "react";
 
 export default function Home() {
   const router = useRouter();
+
+  const [tall, setTall] = useState(0);
+  const [weight, setWeight] = useState(0);
+  const [gender, setGender] = useState(0);
+  const [age, setAge] = useState(0);
+
+  const submit = async () => {
+    const basicInfo = {
+      tall: tall,
+      weight: weight,
+      gender: gender,
+      age: age
+    }
+
+    const [response, error] = await postBasicInfo(basicInfo);
+    if(error){
+      console.error(error);
+      alert("basic error");
+      return;
+    }
+    
+    console.log(response);
+    router.push('/cloth');
+  }
+
   return (
     <div className="flex flex-col">
       BI 페이지
@@ -44,7 +71,7 @@ export default function Home() {
         <DropdownOption>a4</DropdownOption>
       </Dropdown>
       </div>
-      <ThemeButton text={"이동하기"} handleClick={()=>{router.push('/cloth')}}/>
+      <ThemeButton text={"이동하기"} handleClick={submit}/>
     </div>
   );
 }
