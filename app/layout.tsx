16a8302ecbfa51152e56d8092e907834dbb27f3a
Mindul <pencake33@naver.com>
2025-06-18 18:45:32 +0900

    레거시 파일 삭제

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,6 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 import './globals.css';
-import Footer from '@/components/Footer';
-import Header from '@/components/Header';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,9 +25,7 @@ export default function RootLayout({
   return (
     <html lang="ko">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased min-h-screen flex flex-col`}>
-        {/* <Header /> */}
         <main className="flex-grow">{children}</main>
-        {/* <Footer /> */}
       </body>
     </html>
   );
